1ca7493e4363bfacad0680342d8a95345a1d7126
Super Villain <anikethgandhari145@gmail.com>
2023-07-23 17:03:43 +0530

    Added the 'added' feature with settimeout function

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -57,6 +57,8 @@ products.forEach((product) => {
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  let addedMessageTimeoutId;
+
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
 
@@ -89,5 +91,21 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     });
 
     document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(
+      ".js-added-to-cart-${product.id}"
+    );
+
+    addedMessage.classList.add("added-to-cart-visible");
+
+    if (addedMessageTimeoutId) {
+      clearTimeout(addedMessageTimeoutId);
+    }
+
+    const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove("added-to-cart-visible");
+    }, 2000);
+
+    addedMessageTimeoutId = timeoutId;
   });
 });
